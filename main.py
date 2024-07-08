275dee5b8908c600d713a2fff6aa476a3b94b24a
Ильфат <56649240+ilfat-kalyamov@users.noreply.github.com>
2024-07-08 18:37:31 +0000

    Полная реструктуризация проекта, разделение на функции и создание привлекательного дебаггинга через редакт сообщений в чате телеграм.

--- main.py
+++ main.py
@@ -1,127 +1,129 @@
-import os
-import telebot
-import shutil
-import requests
-from bs4 import BeautifulSoup
-import re
 import json
+import os
+import re
+import shutil
+from bs4 import BeautifulSoup
+import telebot
+import requests
 from telebot.types import InputFile
 from PIL import Image
 
 bot = telebot.TeleBot(os.environ['BOT_API'])
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    output = "Выполнено действие с ботом в чате: " + str(message.from_user.id) + ' User: ' + str(message.from_user.username)
+def bot_edit_message(bot_message, text):
+    bot_text = bot_message.text + '\n' + text
+    return bot.edit_message_text(chat_id=bot_message.chat.id, message_id=bot_message.message_id, text=bot_text)
+
+def bot_edit_numbers(bot_message, number):
+    bot_text = bot_message.text
+    old = str(number-1) + '/'
+    new = str(number) + '/'
+    bot_text = new.join(bot_text.rsplit(old, 1))
+    return bot.edit_message_text(chat_id = bot_message.chat.id, message_id=bot_message.message_id, text=bot_text)
+
+def parser(message, bot_message):
+    url = message.text
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        new_text = bot_edit_message(bot_message, 'Получен ответ от сервера.')
+    else:
+        new_text = bot_edit_message(bot_message, f'ОШИБКА: Не удалось получить данные: {response.status_code}')
+        return
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    scripts = soup.find_all('script')
+    title = soup.title.string
+    new_text = bot_edit_message(new_text, f'Тайтл: {title}')
+
+    if len(scripts) < 14:
+        new_text = bot_edit_message(new_text, f'ОШИБКА: Не найдено необходимое количество скриптов на сайте. Найдено скриптов: {len(scripts)}')
+        return
+
+    script_14 = scripts[13].string
+    if not script_14:
+        new_text = bot_edit_message(new_text, 'ОШИБКА: Скрипт 14 пуст или не содержит текста.')
+        return
+
+    chapter_pattern = re.search(r'const local_text_epi\s*=\s*\'Ch\.(\d+)\';', script_14)
+    chapter = chapter_pattern.group(1) if chapter_pattern else bot_edit_message(bot_message, 'ОШИБКА: неизвестная глава.')
+    new_text = bot_edit_message(new_text, f'Номер главы: {chapter}')
+
+    img_https_pattern = re.search(r'const imgHttps\s*=\s*(\[[^\]]*\])', script_14)
+    if not img_https_pattern:
+        new_text = bot_edit_message(new_text, 'ОШИБКА: Не удалось обнаружить массив изображений.')
+        return
+    img_https_array = json.loads(img_https_pattern.group(1))
+    new_text = bot_edit_message(new_text, f'Найдено {len(img_https_array)} изображений.')
+    return new_text, chapter, img_https_array
+
+def dir_maker(chapter, bot_message):
+    parent_folder = f'Глава_{chapter}'
+    new_text = bot_edit_message(bot_message, f'Создана папка: {parent_folder}')
+    webp_folder = os.path.join(parent_folder, 'webp')
+    os.makedirs(webp_folder, exist_ok=True)
+    new_text = bot_edit_message(new_text, f'Создана папка для загрузки изображений: {webp_folder}')
+    return new_text, parent_folder, webp_folder
+
+def download_images(img_urls, folder_name, bot_message):
+    new_text = bot_edit_message(bot_message, f'Начинаю загрузку изображений: 0/{len(img_urls)}')
+    for idx, img_url in enumerate(img_urls, 1):
+        response = requests.get(img_url)
+        if response.status_code == 200:
+            img_filename = os.path.join(folder_name, f'{idx}.webp')
+            with open(img_filename, 'wb') as img_file:
+                img_file.write(response.content)
+            new_text = bot_edit_numbers(new_text, idx)
+        else:
+            new_text = bot_edit_message(new_text, f'ОШИБКА: не удалось получить изображение из: {img_url}')
+            return
+    new_text = bot_edit_message(new_text, 'Все изображения загружены.')
+    return new_text
+
+def convert_webp_to_png(src_folder, dst_folder, bot_message, size):
+    new_text = bot_edit_message(bot_message, f'Начата конвертация изображений в png. 0/{size}')
+    os.makedirs(dst_folder, exist_ok=True)
+    n = 0
+    for filename in os.listdir(src_folder):
+        n += 1
+        if filename.endswith(".webp"):
+            webp_image = Image.open(os.path.join(src_folder, filename))
+            png_image = webp_image.convert("RGBA")
+            png_image.save(os.path.join(dst_folder, filename.replace(".webp", ".png")))
+            new_text = bot_edit_numbers(new_text, n)
+        else:
+            new_text = bot_edit_message(new_text, f'ОШИБКА: не удалось конвертировать изображение {filename}')
+    new_text = bot_edit_message(new_text, 'Все изображения конвертированы.')
+    return new_text
+
+def create_and_send_archive(user_id, chapter, bot_message):
+    new_text = bot_edit_message(bot_message, 'Начато создание архива.')
+    parent_folder = f"Глава_{chapter}"
+    shutil.make_archive(parent_folder, format='zip', root_dir=parent_folder)
+    new_text = bot_edit_message(new_text, 'Архив создан. Отправка.')
+    bot.send_document(user_id, InputFile(f'{parent_folder}.zip'))
+    os.remove(f'{parent_folder}.zip')
+    return new_text
+
+def send_message_to_admin(message):
+    output = f"Выполнено действие в чате: {message.from_user.id}\nОт пользователя: {message.from_user.username}\nСообщение: {message.text}"
     bot.send_message(os.environ['ADMIN_ID'], output)
 
-    if message.text == "/start":
-        bot.send_message(message.from_user.id, "Привет, я бот для скачивания глав манги в помощь команде Moon Light team. Отправь мне ссылку на главу и я отправлю тебе в ответ архив. Ссылка пока что работает только для сайта mto.to. Ссылка должна вести на главу в режиме All pages. То есть без части ссылки /1 на конце")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши /start")
-    else:
+@bot.message_handler(commands=['help', 'start'])
+def start_command(message):
+    bot.send_message(message.from_user.id, "Привет, я бот для скачивания глав манги в помощь команде Moon Light team. Отправь мне ссылку на главу и я отправлю тебе в ответ архив. Ссылка пока что работает только для сайта mto.to. Ссылка должна вести на главу в режиме All pages. То есть без части ссылки /1 на конце")
 
-        url = message.text
-        #url = full_url.rpartition('/')[0]
-        print(url)
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.content, 'html.parser')
-            scripts = soup.find_all('script')
-
-            if len(scripts) >= 14:
-                script_14 = scripts[13].string
-
-                if script_14:
-                    img_https_pattern = re.search(r'const imgHttps\s*=\s*(\[[^\]]*\])', script_14)
-
-                    if img_https_pattern:
-                        img_https_array_str = img_https_pattern.group(1)
-                        img_https_array = json.loads(img_https_array_str)
-                        output = str(len(img_https_array)) + ' images found. '
-                        bot.send_message(message.from_user.id, output)
-                        print(len(img_https_array), 'images found.')
-
-                        chapter_pattern = re.search(r'const local_text_epi\s*=\s*\'Ch\.(\d+)\';', script_14)
-
-                        if chapter_pattern:
-                            chapter = chapter_pattern.group(1)
-                            output = 'Chapter ' + chapter
-                            bot.send_message(message.from_user.id, output)
-                            print(f'chapter: {chapter}')
-
-                        parent_folder = "Глава_" + chapter
-                        folder_name = os.path.join(parent_folder, f'webp')
-                        os.makedirs(folder_name, exist_ok=True)
-                        output = 'Folder ' + folder_name + ' created successfully. Downloading pages...'
-                        bot.send_message(message.from_user.id, output)
-                        print(f'Folder "{folder_name}" created successfully')
-
-                        for idx, img_url in enumerate(img_https_array, 1):
-                            try:
-                                img_response = requests.get(img_url)
-
-                                if img_response.status_code == 200:
-                                    img_filename = os.path.join(folder_name, f'{idx}.webp')
-
-                                    # Save the image in PNG format
-                                    with open(img_filename, 'wb') as img_file:
-                                        img_file.write(img_response.content)
-                                    #output = 'Saved ' + img_filename + '.'
-                                    #bot.send_message(message.from_user.id, output)
-                                    print(f'Saved {img_filename}')
-                                else:
-                                    output = 'Failed to retrieve image from ' + img_url + '.'
-                                    bot.send_message(message.from_user.id, output)
-                                    print(f'Failed to retrieve image from {img_url}')
-                            except Exception as e:
-                                output = 'Error downloading ' + img_url + '.'
-                                bot.send_message(message.from_user.id, output)
-                                print(f'Error downloading {img_url}: {e}')
-
-                        png_dir = os.path.join(parent_folder, f'png')
-                        os.makedirs(png_dir, exist_ok=True)
-
-                        for filename in os.listdir(folder_name):
-                            if filename.endswith(".webp"):
-                                webp_image = Image.open(os.path.join(folder_name, filename))
-                                png_image = webp_image.convert("RGBA")
-                                png_image.save(os.path.join(png_dir, filename.replace(".webp", ".png")))
-                                
-                        print('All images saved to the folder.')
-                        output = 'All images saved to the folder.'
-                        bot.send_message(message.from_user.id, output)
-
-                        shutil.rmtree(f'Глава_{chapter}/webp')
-
-                        print('Converting started.')
-                        output = 'Converting started.'
-                        bot.send_message(message.from_user.id, output)
-                        shutil.make_archive('Глава_' + chapter, format='zip', root_dir=parent_folder)
-                        print('ZIP archive created.')
-                        output = 'ZIP archive created. Sending...'
-                        bot.send_message(message.from_user.id, output)
-
-                        bot.send_document(message.from_user.id, InputFile(f'Глава_{chapter}.zip'))
-                        os.remove(f'Глава_{chapter}.zip')
-                        shutil.rmtree(f'Глава_{chapter}')
-
-                    else:
-                        output = 'No imgHttps array found in script 14.'
-                        bot.send_message(message.from_user.id, output)
-                        print('No imgHttps array found in script 14.')
-                else:
-                    output = 'Script 14 is empty or does not contain text.'
-                    bot.send_message(message.from_user.id, output)
-                    print('Script 14 is empty or does not contain text.')
-            else:
-                output = 'Less than 14 script tags found.'
-                bot.send_message(message.from_user.id, output)
-                #print('Less than 14 script tags found.')
-        else:
-            output = f'Failed to retrieve data: {response.status_code}'
-            bot.send_message(message.from_user.id, output)
-            print(f'Failed to retrieve data: {response.status_code}')
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    bot_message = bot.send_message(message.from_user.id, 'Начинаю процесс.')
+    send_message_to_admin(message)
+    bot_message, ch, img_urls = parser(message, bot_message)
+    bot_message, parent_folder, webp_folder = dir_maker(ch, bot_message)
+    bot_message = download_images(img_urls, webp_folder, bot_message)
+    bot_message = convert_webp_to_png(webp_folder, os.path.join(parent_folder, 'png'), bot_message, len(img_urls))
+    shutil.rmtree(webp_folder)
+    bot_message = create_and_send_archive(message.from_user.id, ch, bot_message)
+    shutil.rmtree(parent_folder)
+    return
 
 bot.infinity_polling()
