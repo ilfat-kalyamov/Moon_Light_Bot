0a7ac2a7b60d523b01bb80d8427f60dfaebf5037
Ильфат <56649240+ilfat-kalyamov@users.noreply.github.com>
2024-07-09 21:33:46 +0000

    Фиксы
    Добавлено сообщение о редкой ошибке отправке файла, совпадающей с моментом обновления соединения с ботом.
    Добавлены уникальные папки для каждого чата для того, чтобы действия разных пользователей не пересекались друг с другом.
    Результат работы отправляется ответом на запускаемое сообщение.
    Архив теперь отправляется ответом на сообщение о результате работы.

--- main.py
+++ main.py
@@ -63,11 +63,13 @@ def parser(message, bot_message):
     return new_text, chapter, img_https_array
 
 def dir_maker(chapter, bot_message):
-    parent_folder = f'Глава_{chapter}'
-    new_text = bot_edit_message(bot_message, f'Создаю папку: {parent_folder}')
+    uniq_path = str(bot_message.chat.id)
+    os.makedirs(uniq_path, exist_ok=True)
+    parent_folder = os.path.join(uniq_path, f'Глава_{chapter}')
+    new_text = bot_edit_message(bot_message, f'Создаю папку: Глава_{chapter}')
     webp_folder = os.path.join(parent_folder, 'webp')
     os.makedirs(webp_folder, exist_ok=True)
-    new_text = bot_edit_message(new_text, f'Создаю папку для загрузки изображений: {webp_folder}')
+    new_text = bot_edit_message(new_text, f'Создаю папку для загрузки изображений: Глава_{chapter}/webp')
     return new_text, parent_folder, webp_folder
 
 def download_images(img_urls, folder_name, bot_message):
@@ -102,11 +104,16 @@ def convert_webp_to_png(src_folder, dst_folder, bot_message, size):
     return new_text
 
 def create_and_send_archive(user_id, chapter, bot_message):
-    new_text = bot_edit_message(bot_message, 'Начинаю создание архива.')
-    parent_folder = f"Глава_{chapter}"
+    new_text = bot_edit_message(bot_message, 'Создаю архив.')
+    uniq_path = str(bot_message.chat.id)
+    os.makedirs(uniq_path, exist_ok=True)
+    parent_folder = os.path.join(uniq_path, f'Глава_{chapter}')
     shutil.make_archive(parent_folder, format='zip', root_dir=parent_folder)
     new_text = bot_edit_message(new_text, 'Архив создан. Отправляю.')
-    bot.send_document(user_id, InputFile(f'{parent_folder}.zip'))
+    try:
+        bot.send_document(chat_id=user_id, document=InputFile(f'{parent_folder}.zip'), reply_to_message_id=new_text.message_id)
+    except Exception as e:
+        new_text = bot_edit_message(new_text, f'ОШИБКА: Не удалось отправить файл из-за ошибки со стороны Телеграмм, попытайтесь снова: {e}')
     os.remove(f'{parent_folder}.zip')
     return new_text
 
@@ -116,18 +123,19 @@ def send_message_to_admin(message):
 
 @bot.message_handler(commands=['help', 'start'])
 def start_command(message):
-    bot.send_message(message.from_user.id, "Привет, я бот для скачивания глав манги в помощь команде Moon Light team. Отправь мне ссылку на главу и я отправлю тебе в ответ архив. Ссылка пока что работает только для сайта mto.to. Ссылка должна вести на главу в режиме All pages. То есть без части ссылки /1 на конце")
+    bot.send_message(message.from_user.id, "Привет, я бот для скачивания глав манги в помощь команде Moon Light team. Отправь мне ссылку на главу и я отправлю тебе в ответ архив. Ссылка пока что работает только для сайта mto.to. Ссылка должна вести на главу в режиме All pages. То есть без части ссылки /1 на конце.\n\nПрошу любые сообщения с ошибками отправлять ему @for_what_or")
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     send_message_to_admin(message)
-    bot_message = bot.send_message(message.from_user.id, 'Начинаю процесс.')
+    bot_message = bot.send_message(message.from_user.id, 'Начинаю процесс.', reply_to_message_id=message.message_id)
     bot_message, ch, img_urls = parser(message, bot_message)
     bot_message, parent_folder, webp_folder = dir_maker(ch, bot_message)
     bot_message = download_images(img_urls, webp_folder, bot_message)
     bot_message = convert_webp_to_png(webp_folder, os.path.join(parent_folder, 'png'), bot_message, len(img_urls))
     shutil.rmtree(webp_folder)
     bot_message = create_and_send_archive(message.from_user.id, ch, bot_message)
-    shutil.rmtree(parent_folder)
+    shutil.rmtree(str(bot_message.chat.id))
+    bot_edit_message(bot_message, 'Готово.')
     return
 bot.infinity_polling()
